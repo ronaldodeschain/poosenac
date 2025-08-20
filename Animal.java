0b109e8437620d5a055a1ca6f4bcea54cddead7c
RONALDO LEMOS DA SILVA <01300771003@senacrs.edu.br>
2025-08-19 21:47:10 -0300

    parando por hoje

---
+++ Animal.java
@@ -0,0 +1,40 @@
+public class Animal {
+    private String nome;
+    private String especie;
+    private int idade;
+    private String sexo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+
+}
---
+++ Cliente.java
@@ -0,0 +1,40 @@
+public class Cliente {
+    private String nome;
+    private int telefone;
+    private String endereco;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
