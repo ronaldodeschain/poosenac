49d19ba677b98b399de657c84f072907161ef26d
RONALDO LEMOS DA SILVA <01300771003@senacrs.edu.br>
2025-08-25 20:13:29 -0300

    [25-08] - Exercicio de JOptionPane

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
-
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Main{
     public static void main(String[] args) {
@@ -64,5 +67,36 @@ public class Main{
         servico.setValorServico(35.50);
         System.out.println(servico.getNomeServico() + " - Valor: " + servico.getValorServico());
 
+        System.out.println("---");
+        JOptionPane.showMessageDialog(null, cao);
+        String nome = JOptionPane.showInputDialog(null,"Diga um nome para o nome :P","texto texto",JOptionPane.QUESTION_MESSAGE);
+        JOptionPane.showMessageDialog(null,"voce quis dizer... "+nome);
+
+        JPanel panel = new JPanel();
+        JTextField endereco = new JTextField(15);
+        JTextField bairro = new JTextField(15);
+        JTextField cidade = new JTextField(15);
+        JTextField estado = new JTextField(15);
+        panel.add(new JLabel("Endereco:"));
+        panel.add(endereco);
+        panel.add(new JLabel("Bairro:"));
+        panel.add(bairro);
+        panel.add(new JLabel("Cidade:"));
+        panel.add(cidade);
+        panel.add(new JLabel("Estado:"));
+        panel.add(estado);
+        
+        int result = JOptionPane.showConfirmDialog(null, panel, "Please Enter Your Info", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+        String adress = endereco.getText();
+        String bairronovo = bairro.getText();
+        String emaildoex = cidade.getText();
+        String state = estado.getText();
+        JOptionPane.showMessageDialog(null, "Name: " + adress + "\nAge: " + bairronovo + "\nEmail: " + emaildoex + "\nState: " + state);
+            
+        int idade = Integer.parseInt(JOptionPane.showInputDialog(null,"digite a idade: ","idade",JOptionPane.QUESTION_MESSAGE));
+        System.out.println(idade);
+
+}
     }
 }
